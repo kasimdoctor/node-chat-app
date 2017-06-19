f8b365d952be56e9b832c9c477687a423f41a627
Kasim Doctor <kasim.doctor@appdirect.com>
2017-06-18 21:07:16 -0400

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,23 @@ socket.on('disconnect', function () {
     console.log('Disconnected from server.');
 });
 
+function scrollToBottom () {
+    // Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+
+    // Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 // listening for a custom event
 socket.on('newMessage', function (message) {
     var formattedTime = moment(message.createdAt).format('LT');
@@ -19,6 +36,7 @@ socket.on('newMessage', function (message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message) {
@@ -31,6 +49,7 @@ socket.on('newLocationMessage', function(message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (event) {
